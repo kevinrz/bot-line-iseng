1c07cb916364204dc222a4dd0ccbf53081f8b9d6
Kevin Raikhan <kevinerzet@gmail.com>
2019-04-24 08:42:08 +0700

    COba

---
+++ src/test/java/com/bot/woyhemat/AktifitasHandlerTest.java
@@ -0,0 +1,39 @@
+// package com.bot.woyhemat;
+
+// import static org.hamcrest.Matchers.containsString;
+// import static org.hamcrest.Matchers.not;
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+// import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+// import org.springframework.test.context.junit4.SpringRunner;
+
+// @RunWith(SpringRunner.class)
+// @WebMvcTest(controllers = GreetingController.class)
+// public class AKtifitasHandlerTest{
+
+// 	@Test
+// 	public void setPengeluaran() throws Exception{
+
+// 	}
+
+// 	@Test
+// 	public void notifyFacadeNotif() throws Exception{
+
+// 	}
+
+// 	@Test
+// 	public void hapusPengeluaran() throws Exception{
+
+// 	}
+
+
+// }
---
+++ src/test/java/com/bot/woyhemat/ControllerTest.java
@@ -0,0 +1,28 @@
+// package com.bot.woyhemat;
+
+// import static org.hamcrest.Matchers.containsString;
+// import static org.hamcrest.Matchers.not;
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+// import org.junit.Test;
+// import org.junit.runner.RunWith;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+// import org.springframework.test.context.junit4.SpringRunner;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.RequestParam;
+
+// import org.springframework.test.context.junit4.SpringRunner;
+
+// @RunWith(SpringRunner.class)
+// @WebMvcTest(controllers = GreetingController.class)
+// public class ControllerTest{
+
+
+// 	@Test
+// 	public void addObserver() throws Exception{
+
+// 	}
+// }
